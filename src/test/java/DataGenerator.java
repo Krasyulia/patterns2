7e3b577396411e0fd28fd532be3da0b8d13e83e3
Krasyulia <krasjulia@gmail.com>
2023-03-19 12:32:10 +0400

    fixes

--- src/test/java/DataGenerator.java
+++ src/test/java/DataGenerator.java
@@ -1,11 +1,8 @@
 import com.github.javafaker.*;
-import io.restassured.*;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 import static io.restassured.RestAssured.given;
@@ -31,35 +28,6 @@ public class DataGenerator {
             .post("/api/system/users")
             .then()
             .statusCode(200);
-    }
-
-    public static String city() {
-        Faker faker = new Faker(new Locale("ru"));
-        return faker.options().option("Благовещенск", "Архангельск", "Астрахань", "Белгород", "Брянск", "Владимир", "Ульяновск", "Челябинск", "Ярославль");
-    }
-
-    public static String date(int plusDay) {
-        return LocalDate.now().plusDays(plusDay).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-    }
-
-    public static String surNameAndFirstName() {
-        Faker faker = new Faker(new Locale("ru"));
-        return faker.name().lastName() + " " + faker.name().firstName();
-    }
-
-    public static String phone() {
-        Faker faker = new Faker(new Locale("ru"));
-        return faker.phoneNumber().phoneNumber();
-    }
-
-    public static String notCorrectPhone() {
-        Faker faker = new Faker(new Locale("ru"));
-        return faker.options().option("123");
-    }
-
-    public static String notWorkingName() {
-        Faker faker = new Faker(new Locale("ru"));
-        return faker.options().option("Свиридова") + " " + faker.options().option("Алёна");
     }
 
     public static AuthDto user(String status) {
